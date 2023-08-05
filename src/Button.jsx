88b7089bd1cb25aeee7c7542b69e00e32532b1e6
gasparrobi <gasparrobi@gmail.com>
2023-08-05 23:57:20 +0200

    first

---
+++ src/Button.jsx
@@ -0,0 +1,15 @@
+import "./index.scss";
+
+const Button = () => {
+  return (
+    <button className="p-4 flex bg-green-400 text-black">
+      <p>Remote </p>
+      <p>Button</p>
+      <small>
+        <strong className="mx-2">class:</strong>lex bg-green-400 text-black
+      </small>
+    </button>
+  );
+};
+
+export default Button;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  corePlugins: {
+    preflight: false,
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
